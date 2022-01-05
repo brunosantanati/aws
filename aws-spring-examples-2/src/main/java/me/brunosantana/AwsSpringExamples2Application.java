3d34364f1ac7c6ef733b5134a16d145628052857
Bruno Sant' Ana <Bruno.Ana@br.experian.com>
2022-01-05 11:53:32 -0300

    Projeto vazio.

---
+++ aws-spring-examples-2/src/main/java/me/brunosantana/AwsSpringExamples2Application.java
@@ -0,0 +1,13 @@
+package me.brunosantana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringExamples2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringExamples2Application.class, args);
+	}
+
+}
